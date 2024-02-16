7978eb41fa35870a75e583a0c41d3e83d9f6d762
bakhtiyorturaev <bakhtiyor6364@gmail.com>
2024-02-16 19:01:56 +0500

    paginator_page

--- users/views.py
+++ users/views.py
@@ -5,14 +5,36 @@ from django.contrib.auth import authenticate, login, logout
 from .forms import *
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
+
 # Create your views here.
 
 def profiles(request):
+    profile = request.GET.get('q')
     users = Profil.objects.all()
+
+    if profile:
+        users = users.filter(name__icontains=profile)
+
+    page_number = request.GET.get('page')
+    paginator = Paginator(users, 1)
+    users_page = paginator.get_page(page_number)
+
+    max_pages = paginator.num_pages
+    current_page = users_page.number
+    start_page = max(current_page - 5, 1)
+    end_page = min(current_page + 4, max_pages)
+    page_numbers = range(start_page, end_page + 1)
+
     context = {
-        "users": users
+        "users_page": users_page,
+        "page_numbers": page_numbers,
+        "max_pages": max_pages,
+        "current_page": current_page,
     }
     return render(request, 'users/profiles.html', context)
+
 
 def profile(request, id):
     user = Profil.objects.get(id=id)
@@ -25,7 +47,7 @@ def profile(request, id):
 def login_user(request):
     if request.user.is_authenticated:
         return redirect('profiles')
-        
+
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -38,8 +60,8 @@ def login_user(request):
         else:
             messages.error(request, 'Bunday login va parol mavjud emas')
 
-
     return render(request, "users/login.html")
+
 
 def logout_user(request):
     logout(request)
@@ -75,13 +97,14 @@ def register_user(request):
 
     return render(request, "users/register.html", context)
 
+
 @login_required(login_url='login')
 def account(request):
     profil = request.user.profil
     context = {
         "profil": profil
     }
-    return render(request,"users/account.html", context)
+    return render(request, "users/account.html", context)
 
 
 @login_required(login_url='login')
@@ -97,4 +120,4 @@ def account_edit(request):
     context = {
         "form": form
     }
-    return render(request,"users/account_edit.html", context)
+    return render(request, "users/account_edit.html", context)
